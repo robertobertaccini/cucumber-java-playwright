e553c99e7b80414e8308910f13272944806e7d43
robertobertaccini <roberto.bertaccini@gmail.com>
2025-01-02 16:36:12 +0100

    initial
    iitial

--- run.bat
+++ run.bat
@@ -1,6 +1,5 @@
 set USERNAME="AUTUser@flightmgmt.com"
-set PASSWORD="Merda2024!"
-set TARGET_URL="https://lab-03254-bos.lab-internal.pega.com/prweb/app/FlightManagementSolTest/"
-set SELENIUM_GRID_URL="http://localhost:5555/wd/hub"
+set PASSWORD="PASSWORD"
+set TARGET_URL="SERVER/prweb/app/FlightManagementSolTest/"
 
-mvn test -q -Dbrowser="chrome" -DTargetURL=%TARGET_URL% -DSeleniumGridURL=%SELENIUM_GRID_URL% -Dusername=%USERNAME% -Dpassword=%PASSWORD%
+mvn test -q -DTargetURL=%TARGET_URL% -Dusername=%USERNAME% -Dpassword=%PASSWORD%
